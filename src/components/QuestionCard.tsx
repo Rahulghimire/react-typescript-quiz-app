47e5c6b5f1b3c8b074056ecb607ea6f80a7c3821
Rahul Ghimire <ghimirerahul554@gmail.com>
2024-05-28 08:16:19 +0545

    added-new-feature

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+type Props={
+  question:string;
+  answers:string[];
+  callback:any;
+  userAnswer:boolean;
+  questionNumber:number;
+  totalQuestions:number;
+}
+
+const QuestionCard:React.FC<Props> = ( {question,
+  answers,
+  callback,
+  userAnswer,  
+  questionNumber,
+  totalQuestions}) => { 
+  return (
+    <div>
+      <p>Question:{questionNumber}/{totalQuestions}</p>
+      {answers?.map(answer=>(
+        <div>
+          <button disabled={userAnswer} onClick={callback}></button>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default QuestionCard
